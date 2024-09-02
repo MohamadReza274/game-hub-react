47dd41fbef3111abbc5f1937c9af15bfb57b4129
MohamadReza <reza.mohamadi.98115@gmail.com>
2024-09-02 17:24:58 +0430

    dark, light mode implemented

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
 import Navbar from "@/components/Navbar.tsx";
+import {ThemeProvider} from "@/components/theme-provider.tsx";
 
 function App() {
 
   return (
-    <div className={'grid grid-cols-1'}>
-        <nav className="">
-            <Navbar />
-        </nav>
-        <aside className={"hidden sm:block bg-yellow-400"}>Side bar</aside>
-        <main className={"bg-red-500"}>Main Content</main>
-    </div>
+      <ThemeProvider>
+          <div className={'grid grid-cols-1'}>
+              <nav className="">
+                  <Navbar/>
+              </nav>
+              <aside className={"hidden sm:block bg-yellow-400"}>Side bar</aside>
+              <main className={"bg-red-500"}>Main Content</main>
+          </div>
+      </ThemeProvider>
   )
 }
 
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,13 +1,19 @@
 import Logo from '../assets/14321824_5445208.jpg'
 import {Input} from "@/components/ui/input.tsx";
+import {SunIcon,MoonIcon} from "lucide-react";
+import {useTheme} from "@/components/theme-provider.tsx";
+import {Button} from "@/components/ui/button.tsx";
 
 const Navbar = () => {
+    const {setTheme} = useTheme();
     return (
-        <nav className="flex justify-between items-center w-full py-2">
+        <nav className="flex justify-between items-center w-full p-3">
             <div className={"flex gap-x-2 items-center"}>
-                <img src={Logo} alt={"Logo"} className={"w-12 h-12 rounded-full"}/>
+                <img src={Logo} alt={"Logo"} className={"w-8 h-8 rounded-full"}/>
                 <Input className={'max-w-lg'} placeholder={"Search... "} name={"search"} type={"search"}/>
             </div>
+            <Button className={"hidden dark:flex"} onClick={() => setTheme("light")} variant={"link"}><SunIcon className={"w-5 h-5"} /></Button>
+            <Button className={"dark:hidden"} onClick={() => setTheme("dark")} variant={"link"}><MoonIcon className={"w-5 h-5"} /></Button>
         </nav>
     );
 };
