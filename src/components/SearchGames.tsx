056a824520d44e852e0938d6487a784f5c4f81a1
MohamadReza <reza.mohamadi.98115@gmail.com>
2024-09-07 14:48:36 +0430

    add serach button for mobile Devices

--- src/components/SearchGames.tsx
+++ src/components/SearchGames.tsx
@@ -1,5 +1,7 @@
 import {Input} from "@/components/ui/input.tsx";
 import {useRef} from "react";
+import {Button} from "@/components/ui/button.tsx";
+import {Search} from "lucide-react";
 
 interface Props {
     onSearchGames: (value: string) => void;
@@ -9,7 +11,7 @@ const SearchGames = ({onSearchGames}: Props) => {
     const inputRef = useRef<HTMLInputElement>(null);
 
     return (
-        <form className="w-full" onSubmit={(e) => {
+        <form className="w-full flex gap-x-1" onSubmit={(e) => {
             e.preventDefault()
             if (inputRef.current) {
                 onSearchGames(inputRef.current.value);
@@ -17,6 +19,7 @@ const SearchGames = ({onSearchGames}: Props) => {
         }}>
             <Input ref={inputRef} className={'max-w-3xl w-full'} placeholder={"Search... "} name={"search"}
                    type={"search"}/>
+            <Button className={"w-12"} variant={"outline"}><Search /></Button>
         </form>
     );
 };
